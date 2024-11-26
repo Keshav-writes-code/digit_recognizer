56d4e2665ba9417362f557b91e0baf8141103904
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-11-26 22:42:50 +0530

    refactor: swap out astro icons with use unocss icones preset

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,14 +2,13 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 import svelte from "@astrojs/svelte";
-import icon from "astro-icon";
 
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [UnoCSS({
     injectReset: true,
-  }), svelte(), icon(), tailwind()],
+  }), svelte(), tailwind()],
   site: "https://Keshav-writes-code.github.io",
   base: "digit_recognizer",
 });
