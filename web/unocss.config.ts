bce0b630882eb3d3ed8c750cf8544f30a3fd2961
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-11-26 22:44:28 +0530

    use unocss icons preset for icons

--- web/unocss.config.ts
+++ web/unocss.config.ts
@@ -2,10 +2,12 @@
 import { defineConfig } from 'unocss'
 import { presetUno } from 'unocss'
 import {transformerVariantGroup} from 'unocss'
+import { presetIcons } from 'unocss'
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetIcons(),
   ],
   transformers: [
     transformerVariantGroup(),
